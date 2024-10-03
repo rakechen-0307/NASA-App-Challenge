3b19bc2362f809ebac6d166a097e0b384b163799
ac618 <andychou618@gmail.com>
2024-10-03 23:18:55 +0800

    detection code done

---
+++ seismic_detection.m
@@ -0,0 +1,128 @@
+%% Initialize
+% close all
+clear;
+close all;
+clc;
+% change directory
+script_directory = fileparts(mfilename('fullpath'));
+cd(script_directory);
+fprintf("Working directory: %s.\n", script_directory);
+% Prepare plot
+figure(1);
+sgtitle("Seismic Detection Process");
+plot_count = 5;
+line_width = 1.2;
+
+%% Hyperparameters
+frequency_band = [0.5 2]; % band for the band-pass filter
+smoothing_std = 6e2; % std of the Gaussian smoothing
+peak_std_num = 2; % number of std to be recognized as a peak (recommend 2~3)
+slope_threshold = 5e-13; % largest slope (abs) to be recognized a a quake
+slope_ratio_threshold = 2;
+%% Read File
+% Moon
+data_directory = fullfile("data", "lunar", "training", "data", "S12_GradeA");
+filename = "xa.s12.00.mhz.1970-07-20HR00_evid00011.csv";
+% filename = "xa.s12.00.mhz.1970-10-24HR00_evid00014.csv";
+% filename = "xa.s12.00.mhz.1973-03-01HR00_evid00093.csv";
+
+% Mars
+% data_directory = fullfile("data", "mars", "test", "data");
+% filename = "XB.ELYSE.02.BHV.2022-05-04HR23_evid0001.csv";
+
+file_path = fullfile(data_directory, filename);
+% read data
+fprintf("Reading data...\n")
+data_table = readtable(file_path, 'VariableNamingRule', 'preserve');
+fprintf("Data read.\n")
+time = data_table{:, 2};
+velocity = data_table{:, 3};
+velocity_original = velocity; % save a copy
+ts = time(2) - time(1); % sampling time
+% plot original signal
+subplot(plot_count, 1, 1);
+plot(time, velocity, "LineWidth", line_width);
+title("Original Signal"); xlabel("Time (s)"); ylabel("Velocity (m/s)");
+
+%% Step 1: Bandpass Filter
+velocity = bandpass(velocity, frequency_band, 1 / ts);
+subplot(plot_count, 1, 2)
+plot(time, velocity, "LineWidth", line_width)
+title("Step1: Bandpass Filter"); xlabel("Time (s)"); ylabel("Velocity (m/s)");
+
+%% Step 2: Absolute Value + Smoothing + Normalization
+% Absolute value
+velocity = abs(velocity);
+% Gaussian smoothing
+window_size = 6 * smoothing_std;
+x = -floor(window_size / 2):1:floor(window_size / 2);
+gaussian_kernel = exp(-x .^ 2 / (2 * smoothing_std ^ 2));
+gaussian_kernel = gaussian_kernel / sum(gaussian_kernel);
+velocity = conv(velocity, gaussian_kernel, 'same');
+
+% Normalization
+average = mean(velocity);
+velocity = velocity - average;
+velocity = velocity .* (velocity >= 0);
+
+% Plot
+subplot(plot_count, 1, 3);
+plot(time, velocity, "LineWidth", line_width);
+title("Step2: Absolute Value + Smoothing + Normalization"); xlabel("Time (s)"); ylabel("Velocity (m/s)");
+
+%% Step 3: Find Peaks + Slopes
+% Find peaks
+[peaks, peak_indices, level] = peaksfinder(velocity, peak_std_num, 0.3);
+subplot(plot_count, 1, 4); hold on;
+plot(time, velocity, "LineWidth", line_width);
+title("Step3: Find Peaks & Slopes"); xlabel("Time (s)"); ylabel("Velocity (m/s)");
+xline(peak_indices(:, 2) * ts, "LineWidth", line_width, "Color", "red");
+yline(level, "LineWidth", line_width, "Color", "cyan");
+% Slopes
+left_slopes = zeros(length(peak_indices), 1);
+right_slopes = zeros(length(peak_indices), 1);
+slope_ratios = zeros(length(peak_indices), 1);
+for i = 1:size(peak_indices, 1)
+    left = peak_indices(i, 1);
+    mid = peak_indices(i, 2);
+    right = peak_indices(i, 3);
+    left_slope = (velocity(mid) - velocity(left)) / (mid - left);
+    right_slope = (velocity(right) - velocity(mid)) / (right - mid);
+    ratio = abs(left_slope / right_slope);
+    % Store results
+    left_slopes(i) = left_slope;
+    right_slopes(i) = right_slope;
+    slope_ratios(i) = ratio;
+end
+% Plot
+for i = 1:size(peak_indices, 1)
+    % slope
+    plot(ts * peak_indices(i, :), velocity(peak_indices(i, :)), "LineWidth", line_width, "Color", "green" , "LineStyle", "--");
+    % ratio
+    x0 = peak_indices(i, 2);
+    x1 = x0 + slope_ratios(i) * 5e3;
+    y0 = peaks(i);
+    plot(ts * [x0, x1], [y0, y0], "LineWidth", line_width, "Color", "magenta")
+end
+hold off;
+
+% Step 4: Select the Peaks
+peaks = [];
+slopes = [];
+for i = 1:size(peak_indices, 1)
+    if (abs(right_slopes(i)) < slope_threshold) && (slope_ratios(i) > slope_ratio_threshold)
+        peaks = [peaks peak_indices(i, 1)];
+        slopes = [slopes left_slopes(i)];
+    end
+end
+subplot(plot_count, 1, 5); hold on;
+plot(time, velocity_original, "LineWidth", line_width);
+if ~isempty(peaks)
+    for i = 1:length(peaks)
+        x1 = peaks(i);
+        y1 = velocity(x1);
+        x0 = x1 - y1 / slopes(i);
+        xline(x0 * ts, "LineWidth", line_width, "Color", "red");
+    end
+end
+title("Step4: Pick Peaks"); xlabel("Time (s)"); ylabel("Velocity (m/s)");
